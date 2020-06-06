87bb315c21a89191c340b6fdfd0968ac1d606662
Franz Diebold <franz.diebold@tum.de>
2020-06-06 16:13:03 +0200

    Add usage info to schematics output.

--- projects/ng-google-sheets-db/schematics/ng-add/index.ts
+++ projects/ng-google-sheets-db/schematics/ng-add/index.ts
@@ -4,6 +4,9 @@ import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 export function ngAdd(): Rule {
   return (tree: Tree, context: SchematicContext) => {
     context.addTask(new NodePackageInstallTask());
+
+    context.logger.info('ℹ️ You need to add "GoogleSheetsDbService" to your app\'s module as a "provider" and Angular\'s "HttpClientModule" to the "imports".');
+
     return tree;
   };
 }
